8a87f1c89ec2083d87dec2005c6a1bb69c4517e1
Tuấn Lê <Leanhtuan007243@gmail.com>
2019-04-05 02:13:23 +0700

    trang 30

--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+
 
 namespace BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
@@ -28,3 +45,5 @@ namespace BigSchool.Controllers
         }
     }
 }
+
+//trang 30
